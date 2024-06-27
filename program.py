2b0dc9f1ff40d68c6c7d421a0633969db2db0ab9
therynate <renajok31@gmail.com>
2024-06-27 15:23:23 +1000

    Код программы

---
+++ program.py
@@ -0,0 +1,7 @@
+def convert_case():
+    user_input = input("Введите строку: ")
+    lower_case = user_input.lower()
+    upper_case = user_input.upper()
+    print(f"Строка в нижнем регистре: {lower_case}")
+    print(f"Строка в верхнем регистре: {upper_case}")
+convert_case()
