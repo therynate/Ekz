da51118a8c3240083de18e8b3bca771e28b4870d
therynate <renajok31@gmail.com>
2024-06-27 15:24:11 +1000

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,15 @@
 def convert_case():
+    # Запрос строки у пользователя
     user_input = input("Введите строку: ")
+
+    # Преобразование строки в нижний регистр
     lower_case = user_input.lower()
+    # Преобразование строки в верхний регистр
     upper_case = user_input.upper()
+
+    # Вывод результатов
     print(f"Строка в нижнем регистре: {lower_case}")
     print(f"Строка в верхнем регистре: {upper_case}")
+
+# Запуск функции
 convert_case()
